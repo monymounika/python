509aa1e24de2ea4d4c587cc69b29b485540f1df2
mony <mounikadevi.rayudu@gmail.com>
2022-12-05 14:44:58 +0530

    second commit

--- product.py
+++ product.py
@@ -2,8 +2,9 @@ totalProduct=0
 product1=int(input("Enter amount for product 1:"))
 product2=int(input("Enter amount for product 2:"))
 product3=int(input("enter amount for product 3:"))
-product1Amount=product1*40
+product1Amount=product1*400
 product2Amount=product1*50
 product3Amount=product1*60
 totalProduct=product1Amount+product2Amount+product3Amount
 print("print the total the user needs to pay=", totalProduct)
+
