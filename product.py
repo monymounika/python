2aff1e8e4c591c6aa860ec8e749f163c5771c233
monymounika <84061652+monymounika@users.noreply.github.com>
2022-12-09 11:52:13 +0530

    Add files via upload

--- product.py
+++ product.py
@@ -8,3 +8,14 @@ product3Amount=product1*60
 totalProduct=product1Amount+product2Amount+product3Amount
 print("print the total the user needs to pay=", totalProduct)
 
+if((product1<=0) or (product2<=0) or (product3<=0)):
+    print("please enter a positive number")
+else:
+    totalProduct=product1Amount+product2Amount+product3Amount
+    entries={product1:100, product2:200, product2Amount:300}
+    print("the amount of all products")
+    entries1={product1:product1Amount, product2:product2Amount, product3:product3Amount}
+    for k,v in enteries.items():
+        print(k,v)
+    print(entries1)
+    print("the total amount should be paid by the user is", totalProduct)
