423125323c7e7edea61f6e9d342e273c9307a50b
monymounika <84061652+monymounika@users.noreply.github.com>
2022-12-09 12:03:35 +0530

    Add files via upload

--- product.py
+++ product.py
@@ -1,21 +1,20 @@
-totalProduct=0
 product1=int(input("Enter amount for product 1:"))
 product2=int(input("Enter amount for product 2:"))
 product3=int(input("enter amount for product 3:"))
-product1Amount=product1*400
-product2Amount=product1*50
-product3Amount=product1*60
-totalProduct=product1Amount+product2Amount+product3Amount
-print("print the total the user needs to pay=", totalProduct)
-
 if((product1<=0) or (product2<=0) or (product3<=0)):
     print("please enter a positive number")
 else:
-    totalProduct=product1Amount+product2Amount+product3Amount
-    entries={product1:100, product2:200, product2Amount:300}
+    print("the product quality with price: ")
+    total=product1*200 + product2*300 + product3*400
+    entries= {product1: 200, product2: 300, product3:400}
+    print(entries)
+    x=open("data.txt","a")
     print("the amount of all products")
-    entries1={product1:product1Amount, product2:product2Amount, product3:product3Amount}
-    for k,v in enteries.items():
-        print(k,v)
-    print(entries1)
-    print("the total amount should be paid by the user is", totalProduct)
+    print("the amount of all products",file=x)
+    for i,p in entries.items():
+        print(i,p)
+        print(i,p,file=x)
+    print("the amount that you need: ")
+    print("the amount that you need: ",file=x)
+    print(total)
+    print(total,file=x)
